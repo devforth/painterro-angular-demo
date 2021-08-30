b453882e07a88e786d2af1ae72acbed2370013b9
Ivan Borshchov <ivan@devforth.io>
2021-08-30 21:52:49 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// @ts-ignore: Unreachable code error
+import Painterro from 'painterro';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pad';
+  ngOnInit() {
+    Painterro().show();
+  }
 }
+
